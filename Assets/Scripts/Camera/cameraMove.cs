914e8a3b01c36342c753416fa37e9f449bd6bbe2
Abigailoby <Abigailirfan7@gmail.com>
2023-10-23 06:27:09 +0700

    Camera Move
    camera ngikutin player

---
+++ Assets/Scripts/Camera/cameraMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
+
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
+    }
+}
