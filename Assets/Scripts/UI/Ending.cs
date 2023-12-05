2f6bb88b613c69ab489ba01cf41fd9b3678cda2b
Abigailoby <Abigailirfan7@gmail.com>
2023-12-06 05:23:21 +0700

    update
    .

---
+++ Assets/Scripts/UI/Ending.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Ending : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    bool isPaused = false;
+    double pausedTime;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    private void Update()
+    {
+        // Menyimpan status pause game
+        isPaused = Time.timeScale == 0f;
+
+        // Menjalankan video jika game tidak di-pause
+        if (!isPaused)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            videoPlayer.Pause();
+        }
+
+
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        Debug.Log("Video Selesai");
+        // Tambahkan aksi yang diinginkan setelah video selesai
+        // Contoh: Pindah ke scene lain
+        SceneManager.LoadScene("StartMenu");
+    }
+}
